c6b03f01ad55b1a54cf1be8de6f801a3242ecd77
iamclement1 <blessedmarcel1@gmail.com>
2021-07-25 20:04:58 +0100

    changed login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,57 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
 
 export default function LoginPage() {
   return (
-    <main className="h-screen flex justify-center items-center bg-green-300">
-      <div className="flex flex-col relative w-full md:m-20">
-        <div className="bg-green-800 text-white sm:rounded-md p-5 m-18 shadow-2xl mt-7 ">
-          <h1 className="text-4xl">Welcome Back</h1>
-          <p className="text-xs">
-            Student Project Allocation Management System
-          </p>
-          <p className="text-xs sm:mt-32">Department of Computer Science</p>
+    <div className="h-screen px-36 bg-white">
+      {/** Header */}
+      <header className="flex justify-between py-8">
+        <div>
+          <img src={Logo} alt="Logo" className="h-16 w-14" />
         </div>
-        <div className="sm:absolute md:absolute p-5 right-12 bg-white sm:rounded-md p-2 sm:w-70 flex flex-col justify-center items-center shadow-xl">
-          <h2 className="text-green-800 mb-3 md:text-3xl font-semibold ">
-            Log In
+        <div className="text-black">
+          Student Project Allocation Management System
+        </div>
+      </header>
+
+      <main className="pt-24 flex justify-center">
+        <div className="w-full sm:w-1/2 md:w-3/5 lg:w-3/6 py-6 border border-gray-300">
+          <h1 className="text-2xl text-center font-bold text-green-900">
+            Welcome Back
+          </h1>
+          <h2 className="text-center text-green-800">
+            Lorem ipsum dolor sit amet.
           </h2>
-          <div className="bg-green-800 shadow-xl rounded-full w-12 h-12"></div>
-          <input
-            type="text"
-            placeholder="Enter Username"
-            className="text-xs w-full p-1 focus:outline-none border-b-2 border-grey-600 focus:border-green-800 bg-transparent mb-2"
-          />
-          <input
-            type="text"
-            placeholder="*************"
-            className="text-xs w-full p-1 focus:outline-none border-b-2 border-grey-600 focus:border-green-800 bg-transparent"
-          />
-          <div className="inline-block relative w-full mt-3">
-            <select className="text-xs appearance-none w-full bg-white border border-green-800 hover:border-green-500 px-4 py-2 pr-8 sm:rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-              <option>--Login As--</option>
-              <option>Student</option>
-              <option>Supevisor</option>
-              <option>Admin</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                class="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
+          {/* username input */}
+          <div className="flex justify-center">
+            <div className="w-full md:w-1/2 ">
+              <div className="justify-center w-full pt-12 max-w-xs px-8">
+                <label htmlFor="" className="block font-semibold">
+                  Username:
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="Username"
+                />
+              </div>
+              {/* password input */}
+              <div className="justify-center w-full max-w-xs px-8">
+                <label htmlFor="" className="block font-semibold">
+                  Password:
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="**********"
+                />
+              </div>
+
+              {/* login button */}
+              <div className="justify-center w-full max-w-xs px-8 pt-2">
+                <div className=" justify-center text-center">
+                  <button className="bg-green-900 text-white p-2 hover:bg-green-600 w-full rounded">
+                    Login
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-          <button className="bg-green-800 hover:bg-green-400 text-xs text-white w-full mt-4 mb-7 focus:outline-none p-2">
-            Login
-          </button>
-          <h2>
-            Not registered? <Link to="/register">Register here</Link>
-          </h2>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
